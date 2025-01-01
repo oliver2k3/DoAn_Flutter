7ac7c1dee4ad556fdb9386f07144d527ca57e16f
oliver2k3 <olivergaming9889@gmail.com>
2025-01-01 12:43:16 +0700

    home screen

---
+++ lib/util/constant.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const COLOR_PRIMARY = Colors.deepOrangeAccent;
+const COLOR_ACCENT = Colors.orange;
+const COLOR_BACKGROUND_DARK = Color(0xFF171822);
+const COLOR_BACKGROUND = Colors.white;
+const COLOR_BACKGROUND_LIGHT = Color(0xFFF1F3F6);
+
+ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: COLOR_PRIMARY,
+  cardColor: const Color(0xffF1F3F6),
+  iconTheme: const IconThemeData(
+    color: Color(0xFF3A4276),
+  ),
+  textTheme: TextTheme(
+    labelLarge: GoogleFonts.poppins(
+      fontSize: 15,
+      height: 1.6,
+      color: const Color(0xff212330),
+      fontWeight: FontWeight.w600,
+    ),
+    bodyMedium: GoogleFonts.poppins(
+      fontSize: 12,
+      color: const Color(0xff1B1D28),
+      fontWeight: FontWeight.w500,
+    ),
+    bodyLarge: GoogleFonts.poppins(
+      height: 1.6,
+      fontSize: 12,
+      color: const Color(0xff7b7f9e),
+      fontWeight: FontWeight.w400,
+    ),
+    displayMedium: GoogleFonts.poppins(
+      fontSize: 24,
+      color: const Color(0xff171822),
+      fontWeight: FontWeight.w600,
+    ),
+    displaySmall: GoogleFonts.poppins(
+      fontSize: 22,
+      color: const Color(0xff3A4276),
+      fontWeight: FontWeight.w800,
+    ),
+    headlineMedium: GoogleFonts.poppins(
+      fontSize: 15,
+      color: const Color(0xff3A4276),
+      fontWeight: FontWeight.w600,
+    ),
+    headlineSmall: GoogleFonts.poppins(
+      fontSize: 22,
+      color: Colors.black,
+      fontWeight: FontWeight.w600,
+    ),
+    titleLarge: GoogleFonts.poppins(
+      fontSize: 20,
+      color: Colors.black,
+      fontWeight: FontWeight.w500,
+    ),
+    titleMedium: GoogleFonts.poppins(
+      fontSize: 18,
+      color: Colors.black,
+      fontWeight: FontWeight.w700,
+    ),
+  ),
+  colorScheme: ColorScheme(
+    brightness: Brightness.light,
+    primary: COLOR_PRIMARY,
+    onPrimary: Colors.white,
+    secondary: COLOR_ACCENT,
+    onSecondary: Colors.white,
+    error: Colors.red,
+    onError: Colors.white,
+    background: COLOR_BACKGROUND,
+    onBackground: Colors.black,
+    surface: Colors.white,
+    onSurface: Colors.black,
+  ),
+);
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  iconTheme: const IconThemeData(
+    color: Color(0xff7b7f9e),
+  ),
+  cardColor: const Color(0xFF212330),
+  textTheme: TextTheme(
+    bodyMedium: GoogleFonts.poppins(
+      fontSize: 12,
+      color: const Color(0xffffffff),
+      fontWeight: FontWeight.w500,
+    ),
+    bodyLarge: GoogleFonts.poppins(
+      height: 1.6,
+      fontSize: 12,
+      color: const Color(0xff7b7f9e),
+      fontWeight: FontWeight.w400,
+    ),
+    labelLarge: GoogleFonts.poppins(
+      fontSize: 15,
+      height: 1.6,
+      color: const Color(0xff212330),
+      fontWeight: FontWeight.w600,
+    ),
+    displayMedium: GoogleFonts.poppins(
+      fontSize: 24,
+      color: const Color(0xFFFFFFFF),
+      fontWeight: FontWeight.w600,
+    ),
+    displaySmall: GoogleFonts.poppins(
+      fontSize: 22,
+      color: const Color(0xFFFFFFFF),
+      fontWeight: FontWeight.w800,
+    ),
+    headlineMedium: GoogleFonts.poppins(
+      fontSize: 15,
+      color: const Color(0xff7b7f9e),
+      fontWeight: FontWeight.w600,
+    ),
+    headlineSmall: GoogleFonts.poppins(
+      fontSize: 22,
+      color: Colors.white,
+      fontWeight: FontWeight.w600,
+    ),
+    titleLarge: GoogleFonts.poppins(
+      fontSize: 20,
+      color: Colors.white,
+      fontWeight: FontWeight.w500,
+    ),
+    titleMedium: GoogleFonts.poppins(
+      fontSize: 18,
+      color: Colors.white,
+      fontWeight: FontWeight.w700,
+    ),
+  ),
+  colorScheme: ColorScheme(
+    brightness: Brightness.dark,
+    primary: COLOR_PRIMARY,
+    onPrimary: Colors.black,
+    secondary: COLOR_ACCENT,
+    onSecondary: Colors.black,
+    error: Colors.red,
+    onError: Colors.black,
+    background: COLOR_BACKGROUND_DARK,
+    onBackground: Colors.white,
+    surface: Colors.black,
+    onSurface: Colors.white,
+  ),
+);
